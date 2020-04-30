3183954aa6af7ffc9db5c665642f95b85f54a8e3
FahimMuntashir <fahim.tech.me@gmail.com>
2020-04-30 23:42:31 +0600

    Upper  Design

---
+++ lib/constraints.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+
+Color lightGreen = Color(0xFF95E08E);
+Color lightBlueIsh = Color(0xFF33BBB5);
+Color darkGreen = Color(0xFF00AA12);
+Color backgroundColor = Color(0xFFEFEEF5);
+
+
+TextStyle titleStyleWhite = new TextStyle(
+    fontFamily: 'Helvetica',
+    color: Colors.white,
+    fontWeight: FontWeight.bold,
+    fontSize: 25
+);
+TextStyle jobCardTitileStyleBlue = new TextStyle(
+    fontFamily: 'Avenir',
+    color: lightBlueIsh,
+    fontWeight: FontWeight.bold,
+    fontSize: 12
+);
+TextStyle jobCardTitileStyleBlack = new TextStyle(
+    fontFamily: 'Avenir',
+    color: Colors.black,
+    fontWeight: FontWeight.bold,
+    fontSize: 12
+);
+TextStyle titileStyleLighterBlack = new TextStyle(
+    fontFamily: 'Avenir',
+    color: Color(0xFF34475D),
+    fontWeight: FontWeight.bold,
+    fontSize: 20
+);
+
+TextStyle titileStyleBlack = new TextStyle(
+    fontFamily: 'Helvetica',
+    color: Colors.black,
+    fontWeight: FontWeight.bold,
+    fontSize: 20
+);
+TextStyle salaryStyle = new TextStyle(
+    fontFamily: 'Avenir',
+    color: darkGreen,
+    fontWeight: FontWeight.bold,
+    fontSize: 12
+);
+
+
+
+
+
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,88 +1,81 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(CovidUI());
+import 'constraints.dart';
+void main() => runApp(MyApp());
 
-class CovidUI extends StatelessWidget {
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: UserPage(),
-      theme: ThemeData().copyWith(
-        primaryColor: Colors.teal,
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primaryColor: Color(0xFFCDE3CE),
       ),
+      home: MyHomePage(),
     );
   }
 }
 
-class UserPage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Center(
-          child: Text(
-            'Covid 19',
-            style: TextStyle(),
-          ),
-        ),
+        title: Center(child: Text('Covid 19')),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          ClipPath(
-            clipper: MyClipper(),
-            child: Container(
-              height: 300.0,
-              width: double.infinity,
+      body: Container(
+        color: backgroundColor,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.all(40),
+              constraints: BoxConstraints.expand(height: 225),
               decoration: BoxDecoration(
-                gradient: LinearGradient(
-                    begin: Alignment.topRight,
-                    end: Alignment.bottomLeft,
-                    colors: [
-                      Color(0xFF20BF55),
-                      Color(0xFF01BAEF),
-                    ]),
-              ),
-              child: Column(
-                children: <Widget>[
-
-                  Expanded(
-                    child: Stack(
-                      children: <Widget>[
-                        Center(
-                          child: Image(
-                            image: AssetImage("images/logo.png"),
-                          ),
-                        ),
-                      ],
+                  gradient: new LinearGradient(
+                      colors: [lightBlueIsh, lightGreen],
+                      begin: const FractionalOffset(1.0, 1.0),
+                      end: const FractionalOffset(0.2, 0.2),
+                      stops: [0.0, 1.0],
+                      tileMode: TileMode.clamp),
+                  borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(30),
+                      bottomRight: Radius.circular(30))),
+              child: Container(
+                padding: EdgeInsets.only(top: 30),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      'Stay Home,\n Stay Safe',
+                      style: titleStyleWhite,
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
-          ),
-        ],
+            SizedBox(
+              height: 20.0,
+            ),
+            Center(
+              child: Text(
+                'Symptoms',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 28.0,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
-  }
-}
-
-class MyClipper extends CustomClipper<Path> {
-  @override
-  Path getClip(Size size) {
-    var path = Path();
-    path.lineTo(0, size.height - 80);
-    path.quadraticBezierTo(
-        size.width / 2, size.height, size.width, size.height - 80);
-    path.lineTo(size.width, 0);
-    path.close();
-    return path;
-  }
-
-  @override
-  bool shouldReclip(CustomClipper<Path> oldClipper) {
-    // TODO: implement shouldReclip
-    return false;
   }
 }
